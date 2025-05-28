c795c6a82ca5a6f21a0d3a7e77fc0d7763abe648
김기진 <abjin69@gmail.com>
2025-05-28 21:14:35 +0900

    feat: add langgraph

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { SlackModule } from 'nestjs-slack-bolt';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SlackModule } from 'nestjs-slack-bolt';
+import { ConversationsModule } from './conversations/conversations.module';
+import { NotionModule } from './notion/notion.module';
+import { GithubModule } from './github/github.module';
+import { ModelModule } from 'model/model.module';
 
 @Module({
-  imports: [SlackModule.forRoot()],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SlackModule.forRoot({
+      signingSecret: process.env.SLACK_SIGNING_SECRET,
+      token: process.env.SLACK_BOT_TOKEN,
+    }),
+    ConversationsModule,
+    NotionModule,
+    GithubModule,
+    ModelModule,
+  ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
