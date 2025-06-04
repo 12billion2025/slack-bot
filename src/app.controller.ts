b611ba519759c031f69e02c605590cfdfe1e397a
김기진 <abjin69@gmail.com>
2025-06-04 16:32:46 +0900

    feat: migrate ws to http

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,45 @@
-import { Controller } from '@nestjs/common';
-import { Event } from 'nestjs-slack-bolt';
-import { SlackEventMiddlewareArgs } from '@slack/bolt';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { WebClient } from '@slack/web-api';
+import { ConfigService } from '@nestjs/config';
+import { SlackEventSubscription } from '@types';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private slackClient: WebClient;
 
-  @Event('app_mention')
-  async event({ event, say }: SlackEventMiddlewareArgs<'app_mention'>) {
+  constructor(
+    private readonly appService: AppService,
+    private readonly config: ConfigService,
+  ) {
+    this.slackClient = new WebClient(this.config.get('SLACK_BOT_TOKEN'));
+  }
+
+  @Post()
+  async subscribeSlackEvent(
+    @Body() { event, challenge }: SlackEventSubscription,
+  ) {
     try {
       const reply = await this.appService.createCompletions(
         event.text,
         event.channel,
         event.thread_ts || event.ts,
       );
-      await say({ text: reply, thread_ts: event.ts });
-    } catch (error) {
-      await say({
-        text: '오류가 발생했습니다. 다시 시도해주세요.',
+
+      this.slackClient.chat.postMessage({
+        channel: event.channel,
         thread_ts: event.ts,
+        text: reply,
+      });
+    } catch (error) {
+      this.slackClient.chat.postMessage({
+        channel: event.channel,
+        thread_ts: event.ts,
+        text: '오류가 발생했습니다. 다시 시도해주세요.',
       });
       console.error(error);
+    } finally {
+      return challenge;
     }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { SlackModule } from 'nestjs-slack-bolt';
 import { ConversationsModule } from './conversations/conversations.module';
 import { NotionModule } from './notion/notion.module';
 import { ScheduleModule } from '@nestjs/schedule'; // notion을 위한 스케줄러 모듈
@@ -13,10 +12,6 @@ import { EmbeddingModule } from './embedding/embedding.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    SlackModule.forRoot({
-      signingSecret: process.env.SLACK_SIGNING_SECRET,
-      token: process.env.SLACK_BOT_TOKEN,
-    }),
     ScheduleModule.forRoot(), // notion을 위한 스케줄러 모듈
     ConversationsModule,
     NotionModule,
